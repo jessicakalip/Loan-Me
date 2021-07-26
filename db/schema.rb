f2344c6840d1cd6168492f5c398cb137c5c3c45b
Yitang Wang <yitangwang0@gmail.com>
2021-07-26 18:06:47 +0800

    Creat seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_080209) do
+ActiveRecord::Schema.define(version: 2021_07_26_093938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,10 +33,10 @@ ActiveRecord::Schema.define(version: 2021_07_26_080209) do
     t.integer "age"
     t.string "interest"
     t.float "average_rating"
-    t.bigint "users_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["users_id"], name: "index_profiles_on_users_id"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2021_07_26_080209) do
 
   add_foreign_key "hangouts", "profiles"
   add_foreign_key "hangouts", "users"
-  add_foreign_key "profiles", "users", column: "users_id"
+  add_foreign_key "profiles", "users"
   add_foreign_key "reviews", "hangouts"
   add_foreign_key "reviews", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# 50 users
+# only 20 users has profils
+# users.sample booking for the profile
+# some of the booking has review
+Review.destroy_all
+Hangout.destroy_all
+Profile.destroy_all
+User.destroy_all
+
+puts "creating fake users..."
+50.times do |number|
+  user = User.new(email: "email_#{number}@gmail.com", password: "password")
+  user.save!
+end
+
+puts "creating fake profiles..."
+User.all.sample(20).each do |user|
+  profile = Profile.new(
+             name: Faker::Name.name,
+             gender: Faker::Gender.binary_type,
+             age: rand(18..40),
+             interest: Faker::Hobby.activity,
+             average_rating: rand(1.0..5.0).round(2),
+             user: user)
+  profile.save!
+  rand(5..10).times do
+    future = [true, false].sample
+    date = future ? Date.today + rand(1..10) : Date.today - rand(10..20)
+    status = future ? ["pending", "reject", "accept"].sample : "completed"
+    puts "creating hangouts....."
+    hangout = Hangout.new(
+               status: status,
+               date: date,
+               time: Time.now,
+               profile: profile,
+               user: User.all.sample)
+    hangout.save!
+
+    if hangout.status == "completed"
+      puts "creating reviews....."
+      review = Review.new(
+                comment: Faker::Restaurant.review,
+                rating: rand(1..5),
+                hangout: hangout,
+                user: hangout.user)
+      review.save!
+    end
+  end
+end
+
+
+
+
+puts "finish"
