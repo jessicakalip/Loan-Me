a24fd2e12e51ec0bbaeeebce92f2756250ebdfa0
Yitang Wang <yitangwang0@gmail.com>
2021-07-27 14:28:27 +0800

    wait for pages show and copey the form on it

--- app/controllers/hangouts_controller.rb
+++ app/controllers/hangouts_controller.rb
@@ -1,8 +1,33 @@
 class HangoutsController < ApplicationController
+  before_action :find_list, only: [:create, :new]
 
    def show
     @id = params[:id]
     @hangout = Hangout.find(@id)
   end
 
+  def new
+    @hangout = Hangout.new
+  end
+
+  def create
+    @hangout = Hangout.new(hangout_params)
+    # link the bookmark to the restaurant
+    @hangout.profile = @profile
+    if @hangout.save
+      redirect_to profile_path(@profile)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_profile
+    @profile = Profile.find(params[:profile_id])
+  end
+
+  def hangout_params
+    params.require(:hangout).permit(:date, :time)
+  end
 end
