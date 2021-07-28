dd58ca4642c4e7b56d97821fbffa47c657363e28
Yves Hurdequint <hurdequinty@hotmail.com>
2021-07-28 13:15:31 +0800

    create review implemented

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @hangout = Hangout.find(params[:hangout_id])
+    @review.hangout = @hangout
+    @review.user = current_user
+    if @review.save
+      redirect_to hangout_path(@hangout)
+    else
+      render "hangouts/show"
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+end
