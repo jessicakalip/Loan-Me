ea6ee177b8e37b273cf22fb25ff05b851e083a85
Yitang Wang <yitangwang0@gmail.com>
2021-08-26 17:05:11 +0800

    input

--- app/javascript/components/init_label.js
+++ app/javascript/components/init_label.js
@@ -2,23 +2,23 @@
 
 
 
-const initLabel = () => {
-  const tagAll = document.querySelectorAll('.tag')
-  // tagAll.forEach((tag) => {
-  //   tag.classList.remove("search-label");
-  //   tag.classList.add("search-select");
-  // });
-  tagAll.forEach((tag) => {
-    tag.addEventListener("click", (event) => {
-       // event.preventDefault();
-       event.currentTarget.classList.add("search-select");
-    });
-  })
-};
+// const initLabel = () => {
+//   const tagAll = document.querySelectorAll('.tag')
+//   // tagAll.forEach((tag) => {
+//   //   tag.classList.remove("search-label");
+//   //   tag.classList.add("search-select");
+//   // });
+//   tagAll.forEach((tag) => {
+//     tag.addEventListener("click", (event) => {
+//        // event.preventDefault();
+//        event.currentTarget.classList.add("search-select");
+//     });
+//   })
+// };
 
 
 
-export { initLabel };
+// export { initLabel };
 
 
 // document.querySelectorAll(".tag").forEach((tag) => {
